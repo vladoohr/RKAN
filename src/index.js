bb9460fe02ed92e1445ae753118b01583181061a
Vladimir Tomanovski <vladoohr@gmail.com>
2017-03-18 18:12:57 +0100

    Fix auth issue

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ import reducers from './reducers'
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducers)
 
+const auth_token = localStorage.getItem('auth_token')
+const user = localStorage.getItem('user')
+if (auth_token) {
+	store.dispatch({
+		type: AUTH_USER,
+		payload: JSON.parse(user)
+	})
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
